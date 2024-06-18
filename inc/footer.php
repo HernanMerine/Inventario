9ac96cd98e715843a9352443c167c908c73e1e01
calubravo <caalu.bravo@gmail.com>
2024-06-17 21:37:27 -0300

    vistas

--- inc/footer.php
+++ inc/footer.php
@@ -1,5 +1,12 @@
    <!-- Footer -->
-   <footer class="footer">
+    <style>
+        .footer-custom{
+            background-color: #363636;
+    color:#fafafa;
+    padding: 3rem 1.5rem 3rem
+        }
+    </style>
+   <footer class="footer-custom">
         <div class="content has-text-centered">
             <p>
                 <strong>Sistema de Inventario y Gestión de Órdenes</strong> por <a href="#">Tu Empresa</a>. 
