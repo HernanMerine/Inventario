9b4350f56adc3058f21a13c5c5a963cdaf31468c
CharlyArbit <140215367+CharlyArbit@users.noreply.github.com>
2024-06-21 19:42:39 -0300

    Fix eliminar usuario

--- php/usuario_eliminar.php
+++ php/usuario_eliminar.php
@@ -1,35 +1,37 @@
 <?php
 
-    /*== Almacenando datos ==*/
-    $user_id_del = limpiar_cadena($_GET['user_id_del']);
+require_once "main.php";
 
-    // Obteniendo la conexión a la base de datos
-    $conexion = conexion();
+/*== Almacenando datos ==*/
+$user_id_del = limpiar_cadena($_GET['user_id_del']);
 
-    if ($conexion->connect_error) {
-        die("Connection failed: " . $conexion->connect_error);
-    }
+// Obteniendo la conexión a la base de datos
+$conexion = conexion();
 
-    /*== Verificando usuario ==*/
-    $check_usuario = $conexion->query("SELECT usuario_id FROM usuario WHERE usuario_id='$user_id_del'");
-    
-    if($check_usuario->num_rows == 1){
+if ($conexion->connect_error) {
+    die("Connection failed: " . $conexion->connect_error);
+}
 
-        $check_productos = $conexion->query("SELECT usuario_id FROM producto WHERE usuario_id='$user_id_del' LIMIT 1");
+/*== Verificando usuario ==*/
+$check_usuario = $conexion->query("SELECT usuario_id FROM usuario WHERE usuario_id='$user_id_del'");
 
-        if($check_productos->num_rows <= 0){
-            
+if ($check_usuario->num_rows == 1) {
+
+    $check_productos = $conexion->query("SELECT usuario_id FROM producto WHERE usuario_id='$user_id_del' LIMIT 1");
+
+    if ($check_productos->num_rows <= 0) {
+        try {
             // Eliminando el usuario
             $eliminar_usuario = $conexion->query("DELETE FROM usuario WHERE usuario_id='$user_id_del'");
 
-            if($eliminar_usuario){
+            if ($eliminar_usuario) {
                 echo '
                     <div class="notification is-info is-light">
                         <strong>¡USUARIO ELIMINADO!</strong><br>
                         Los datos del usuario se eliminaron con éxito
                     </div>
                 ';
-            }else{
+            } else {
                 echo '
                     <div class="notification is-danger is-light">
                         <strong>¡Ocurrió un error inesperado!</strong><br>
@@ -37,22 +39,39 @@
                     </div>
                 ';
             }
-        }else{
-            echo '
-                <div class="notification is-danger is-light">
-                    <strong>¡Ocurrió un error inesperado!</strong><br>
-                    No podemos eliminar el usuario ya que tiene productos registrados por él
-                </div>
-            ';
+        } catch (mysqli_sql_exception $e) {
+            if ($e->getCode() == 1451) {
+                echo '
+                    <div class="notification is-danger is-light">
+                        <strong>¡Ocurrió un error inesperado!</strong><br>
+                        No se pudo eliminar el usuario porque ha cargado órdenes de compra.
+                    </div>
+                ';
+            } else {
+                echo '
+                    <div class="notification is-danger is-light">
+                        <strong>¡Ocurrió un error inesperado!</strong><br>
+                        No se pudo eliminar el usuario, por favor inténtelo nuevamente.
+                    </div>
+                ';
+            }
         }
-        $check_productos->close();
-    }else{
+    } else {
         echo '
             <div class="notification is-danger is-light">
                 <strong>¡Ocurrió un error inesperado!</strong><br>
-                El USUARIO que intenta eliminar no existe
+                No podemos eliminar el usuario ya que tiene productos registrados por él
             </div>
         ';
     }
-    $check_usuario->close();
-    $conexion->close();
+    $check_productos->close();
+} else {
+    echo '
+        <div class="notification is-danger is-light">
+            <strong>¡Ocurrió un error inesperado!</strong><br>
+            El USUARIO que intenta eliminar no existe
+        </div>
+    ';
+}
+$check_usuario->close();
+$conexion->close();
